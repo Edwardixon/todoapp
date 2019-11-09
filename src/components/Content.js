98f9f204c833acdd5bac182ba1d6bb6362a81043
edward <edward@pop-os.localdomain>
2019-11-09 13:56:07 +0200

    fixed css

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,40 +1,37 @@
 import React from 'react';
 import "../styles/Content.css";
+import contentData from "../data";
 
 function Content () {
+    
     return (
         <div className="content-grid">
-            <p >Progress Status</p>
-            <p >Task Name</p>
-            <p >Level</p>
-            <p >Comments</p>
-            <p >Target time</p>
-
-            <div className ="row">
-            <div className="title">
-                <input className="check"type="checkbox" />
-                <p className="">dadada Status</p>
+            <div className="row">
+                <p >Progress Status</p>
+                <p >Task Name</p>
+                <p >Level</p>
+                <p >Comments</p>
+                <p >Target time</p>
             </div>
+            <div className ="row">
+                <div className="title">
+                    <input className="check"type="checkbox" />
+                    <p className="">dadada Status</p>
+                </div>
 
-            <div class="slider-container">
-                <div class="progress">
-                    <div class="bar">
-                        <span class="circle"></span>
+                <div class="slider-container">
+                    <div class="progress">
+                        <div class="bar">
+                            <span class="circle"></span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <p>13</p>
-            <p> Cyka blyat</p>
-            <p>10:20AM</p>
+                <p>13</p>
+                <p> Cyka blyat</p>
+                <p>10:20AM</p>
             </div>
             
-            <div class="slider-container">
-                <div class="progress">
-                    <div class="bar">
-                        <span class="circle"></span>
-                    </div>
-                </div>
-            </div>
+
         </div>
     )
 }
